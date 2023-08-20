b67d0358b1308b9baeac8d80a7e61cb9e5873cae
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2023-08-20 11:58:49 -0500

    Add store db context configuration

--- Store.Api/Program.cs
+++ Store.Api/Program.cs
@@ -1,13 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Store.Db;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreDBContext>(options =>
+{
+    var connectionStr = builder.Configuration.GetConnectionString("Store") ??
+        throw new Exception("Connection string 'Store' not found");
+    options.UseSqlServer(connectionStr);
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetService<StoreDBContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Store.BD/StoreDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store.Db
+{
+    public class StoreDBContext : DbContext
+    {
+        public StoreDBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
