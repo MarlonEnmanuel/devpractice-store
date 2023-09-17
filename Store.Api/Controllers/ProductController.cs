6c4520875e6c031fec90d86651ad10d9fbad8a40
carloscruzado <carlos570570@gmail.com>
2023-09-17 09:39:08 -0500

    producto

---
+++ Store.Api/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Db;
+using Store.Services;
+
+namespace Store.Api.Controllers
+{
+    
+        [ApiController]
+        [Route("api/product")]
+        public class ProductController : Controller
+        {
+            private readonly IProductService _productService;
+
+            public ProductController(IProductService productService)
+            {
+                _productService = productService;
+            }
+
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok(_productService.Get());
+            }
+
+            [HttpPost]
+            public IActionResult Post([FromBody] Product product)
+            {
+                _productService.Save(product);
+                return Ok();
+            }
+
+            [HttpPut("{id}")]
+            public IActionResult Put(int id, [FromBody] Product product)
+            {
+                _productService.Update(id, product);
+                return Ok();
+            }
+
+            [HttpDelete("{id}")]
+            public IActionResult Delete(int id)
+            {
+                _productService.Delete(id);
+                return Ok();
+            }
+        }
+}
---
+++ Store.Services/ProductService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Services
+{
+    public class ProductService: IProductService
+    {
+        private readonly StoreDBContext _context;
+
+        public ProductService(StoreDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Get()
+        {
+            return _context.Products.ToList();
+        }
+
+        public void Save(Product product)
+        {
+            var products = _context.Products.Find(product.Id);
+            if (products == null)
+            {
+                _context.Add(product);
+                _context.SaveChanges();
+            }
+        }
+
+        public void Update(int id, Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+
+            _context.SaveChanges();
+
+        }
+
+        public void Delete(int id)
+        {
+            var producto_encontrado = _context.Products.Find(id);
+            if (producto_encontrado != null)
+            {
+                _context.Remove(producto_encontrado);
+
+                _context.SaveChanges();
+            }
+
+        }
+    }
+
+    public interface IProductService
+    {
+
+        IList<Product> Get();
+
+        void Save(Product product);
+
+        void Update(int id, Product product);
+
+        void Delete(int id);
+    }
+}
