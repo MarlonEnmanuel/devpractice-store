1228d5dcf40c4841a3699d02b5b740c15ccaf4f9
carloscruzado <carlos570570@gmail.com>
2023-10-03 21:12:20 -0500

    update name product  to productDto

--- Store.Services/ProductService.cs
+++ Store.Services/ProductService.cs
@@ -3,14 +3,12 @@ using Microsoft.EntityFrameworkCore;
 using Store.Db;
 using Store.Services.Dtos;
 
-
 namespace Store.Services
 {
     public class ProductService: IProductService
     {
         private readonly StoreDBContext _context;
         private readonly IMapper _mapper;
-
         public ProductService(StoreDBContext context, IMapper mapper)
         {
             _context = context;
@@ -28,50 +26,39 @@ namespace Store.Services
         {
             var product = _mapper.Map<Product>(dto);
             product.Categories = _context.Categories.Where(c => dto.CategoryIds.Contains(c.Id)).ToList();
-
             _context.Products.Add(product);
             _context.SaveChanges();  
         }
 
-        public void Update(int id, SaveProductDto product)
+        public void Update(int id, SaveProductDto productDto)
         {
+            var currentProduct = _context.Products.Find(id);
 
-            var ProductNow = _context.Products.Find(id);
-
-            if (ProductNow != null && ProductNow.Id == product.Id)
+            if (currentProduct != null && currentProduct.Id == productDto.Id)
             {
-                _mapper.Map(product, ProductNow);
+                _mapper.Map(productDto, currentProduct);
 
                 _context.SaveChanges();
-            }
-          
+            }     
         }
 
         public void Delete(int id)
         {
-       
+            var currentProduct = _context.Products.Find(id);
 
-            var productNow = _context.Products.Find(id);
-
-            if (productNow != null)
+            if (currentProduct != null)
             {
-                _context.Remove(productNow);
+                _context.Remove(currentProduct);
                 _context.SaveChanges();
             }
-
-
-
         }
     }
 
     public interface IProductService
     {
         IList<ProductDto> Get();
-
         void Save(SaveProductDto product);
-
         void Update(int id, SaveProductDto product);
-
         void Delete(int id);
     }
 }
