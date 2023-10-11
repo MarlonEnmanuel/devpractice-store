bbea60ce3db9099f639ec0dc104910ec082586ef
alberth270 <36995741+alberth270@users.noreply.github.com>
2023-10-11 09:07:53 -0500

    Apply Dtos in the class Supplier (#15)
    * Crud Provider
    
    * Crud Provider
    
    * Crud Provider
    
    * Crud Provider
    
    * Crud Provider
    
    * Crud Provider
    
    * Apply Dtos in the class Suppliers
    
    * Apply Dtos in the class Suppliers
    
    ---------
    
    Co-authored-by: alberth bances alva <alberth.bances@inetum.com>

--- Store.Api/Controllers/SupplierController.cs
+++ Store.Api/Controllers/SupplierController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Store.Db;
+using Store.Services.Dtos;
 using Store.Services.Interface;
 
 namespace Store.Api.Controllers
@@ -16,35 +16,35 @@ namespace Store.Api.Controllers
         }
 
         [HttpPost]
-        public IActionResult PostProvider(Supplier supplier)
+        public IActionResult PostProvider(SaveSupplierDto supplier)
         {
-            _supplierService.Save(supplier);
+            _supplierService.SaveSupplier(supplier);
             return Ok();
         }
 
         [HttpGet]
         public IActionResult GetProviders()
         {
-            return Ok(_supplierService.Get());
+            return Ok(_supplierService.GetSupplierFindAll());
         }
 
         [HttpGet("{idSupplier}")]
         public IActionResult GetProvidersById(int idSupplier)
         {
-            return Ok(_supplierService.GetById(idSupplier));
+            return Ok(_supplierService.GetSupplierById(idSupplier));
         }
 
         [HttpPut("{idSupplier}")]
-        public IActionResult PutProviders(int idSupplier, Supplier supplier)
+        public IActionResult PutProviders(int idSupplier, SaveSupplierDto supplier)
         {
-            _supplierService.Update(idSupplier, supplier);
+            _supplierService.UpdateSupplier(idSupplier, supplier);
             return Ok();
         }
 
         [HttpDelete]
         public IActionResult DeleteProviders(int idSupplier)
         {
-            _supplierService.Delete(idSupplier);
+            _supplierService.DeleteSupplier(idSupplier);
             return Ok();
         }
 
