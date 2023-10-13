fc95ca0fe81ffe7523ccefcf048e7970edf2ffe8
Gvzt <99850954+Gvzt@users.noreply.github.com>
2023-10-12 21:23:51 -0500

    DPS-16: Brand DTO's (#16)
    * Brand DTO's
    
    * Brand DTOP + refactoring
    
    * Test error
    
    * Error fixed

--- Store.Services/BrandService.cs
+++ Store.Services/BrandService.cs
@@ -1,61 +1,73 @@
-﻿using Store.Db;
+﻿using AutoMapper;
+using Store.Db;
+using Store.Services.Dtos;
 
 namespace Store.Services
 {
     public class BrandService : IBrandService
     {
         private readonly StoreDBContext _context;
+        private readonly IMapper _mapper;
 
-        public BrandService(StoreDBContext dbcontext)
+        public BrandService(StoreDBContext context, IMapper mapper)
         {
-            _context = dbcontext;
+            _context = context;
+            _mapper = mapper;
         }
 
-        public IList<Brand> Get()
+        public IList<BrandDto> GetBrandList()
         {
-            return _context.Brands.ToList();
+            var list = _context.Brands.ToList();
+            return _mapper.Map<IList<BrandDto>>(list);
         }
 
-        public void Save(Brand brand)
+        public void SaveBrand(SaveBrandDto dto)
         {
-            _context.Add(brand);
+            var brand = _mapper.Map<Brand>(dto);
+            
+            _context.Brands.Add(brand);
             _context.SaveChanges();
         }
 
-        public void Update(int id, Brand brand)
+        public void UpdateBrand(int id, SaveBrandDto dto)
         {
-            var BrandNow = _context.Brands.Find(id);
-
-            if (BrandNow != null && BrandNow.Id == brand.Id)
+            if (dto == null || id != dto.Id)
             {
-                BrandNow.Name = brand.Name;
-                BrandNow.Description = brand.Description;
-
-                _context.SaveChanges();
+                return;
             }
+
+            var currentBrand = _context.Brands.Find(id);
+            if (currentBrand == null)
+            {
+                return;
+            }
+
+            _mapper.Map(dto, currentBrand);
+            _context.SaveChanges();
         }
 
-        public void Delete(int id)
+        public void DeleteBrand(int id)
         {
-            var BrandNow = _context.Brands.Find(id);
-
-            if (BrandNow != null)
+            var currentBrand = _context.Brands.Find(id);
+            if (currentBrand == null)
             {
-                _context.Remove(BrandNow);
-                _context.SaveChanges();
+                return;
             }
+
+            _context.Remove(currentBrand);
+            _context.SaveChanges();
         }
     }
-    
+
     public interface IBrandService
     {
-        IList<Brand> Get();
+        IList<BrandDto> GetBrandList();
 
-        void Save(Brand brand);
+        void SaveBrand(SaveBrandDto dto);
 
-        void Update(int id, Brand brand);
+        void UpdateBrand(int id, SaveBrandDto dto);
 
-        void Delete(int id);
+        void DeleteBrand(int id);
     }
 }
 
---
+++ Store.Services/Mappers/BrandMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Store.Db;
+using Store.Services.Dtos;
+
+namespace Store.Services.Mappers
+{
+    internal class BrandMapper : Profile
+    {
+        public BrandMapper()
+        {
+            CreateMap<Brand, BrandDto>();
+            CreateMap<SaveBrandDto, Brand>();
+        }
+    }
+}
