f2cd7fd4e75b4fcf69dc7e283e2dafdf1361fa1a
alberth bances alva <alberth.bances@inetum.com>
2023-10-17 21:29:01 -0500

    Prueba unitaria MapperSupplier

---
+++ Store.Test/Store.Core/Suppliers/SupplierMapperTest.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Store.Core.Modules.Brands.Dtos;
+using Store.Core.Modules.Suppliers;
+using Store.Core.Modules.Suppliers.Dtos;
+using Store.Db.Entities;
+
+namespace Store.Test.Store.Core.Suppliers
+{
+    public class SupplierMapperTest
+    {
+        private readonly IMapper _mapper;
+
+        public SupplierMapperTest()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<SupplierMapper>());
+            _mapper = config.CreateMapper();
+        }
+
+        [Fact]
+        public void Map_Supplier_to_SaveSupplierDto()
+        {
+            var entity = new Supplier
+            {
+                Id = 1,
+                RucSupplier = "10475214581",
+                BusinessName = "Inversiones SA.",
+            };
+
+            var dto = _mapper.Map<SupplierDto>(entity);
+
+            Assert.Equal(entity.Id, dto.Id);
+            Assert.Equal(entity.RucSupplier, dto.RucSupplier);
+            Assert.Equal(entity.BusinessName, dto.BusinessName);
+        }
+    }
+}
