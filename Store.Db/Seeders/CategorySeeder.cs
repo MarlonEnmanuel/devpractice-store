38abf9173253a3dbd8fec85a226c536abcf5b3d2
Gustavo Culqui Vidarte <CulquiVidarteGustavo@gmail.com>
2023-10-17 20:13:14 -0500

    Brand's seed fix

--- Store.Db/Seeders/CategorySeeder.cs
+++ Store.Db/Seeders/CategorySeeder.cs
@@ -16,6 +16,16 @@ namespace Store.Db.Seeders
                 new Category { Id = 5, Name = "Bebidas", Description = ""},
                 new Category { Id = 6, Name = "Bebidas Alcoholicas", Description = ""},
             });
+
+            modelBuilder.Entity<Brand>().HasData(new[]
+            {
+                new Brand { Id = 1, Name = "GLORIA", Description = ""},
+                new Brand { Id = 2, Name = "VOLT", Description = ""},
+                new Brand { Id = 3, Name = "SUBLIME", Description = ""},
+                new Brand { Id = 4, Name = "INKA COLA", Description = ""},
+                new Brand { Id = 5, Name = "DON VICTORIO", Description = ""},
+                new Brand { Id = 6, Name = "ALTOMAYO", Description = ""},
+            });
         }
     }
 }
