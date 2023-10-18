22433d691a7e2b742648e7c2df9b69b7b763151b
carloscruzado <carlos570570@gmail.com>
2023-10-17 20:27:32 -0500

    product

---
+++ Store.Db/Seeders/ProductSeeder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Db.Entities;
+
+namespace Store.Db.Seeders
+{
+    internal class ProductSeeder : ISeeder
+    {
+        public void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new[] 
+            { 
+                new Product { Id = 1,Name="Gloria",Description="leche",Price=10,BrandId=1,Stock=10 }
+
+            });
+
+          
+        }
+    }
+}
