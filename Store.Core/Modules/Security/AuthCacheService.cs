f551d1d6023bb76f3f708b1500c4827746c883b0
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2023-10-19 20:42:48 -0500

    New feature defined, create new class & test

---
+++ Store.Core/Modules/Security/AuthCacheService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Core.Modules.Security
+{
+    // Cache de Tokens
+    // Estructura del token -> { username, claims, roles, expiration }
+
+    // Funcionalidad
+    // 1. Poder registrar el token en la memoria cache
+    // 2. Poder consulta el token desde la memoria
+    // 3. Verificar si el token aun es valido para un usuario
+    // 4. Caducar un token guardado en memoria
+    // 5. Eliminar todos los tokens de la memoria
+    // 6. Obtener todos los tokens de la memoria
+    // 7. Eliminar tokens caducados
+
+    public class AuthCacheService
+    {
+    }
+}
---
+++ Store.Test/Store.Core/Security/AuthCacheServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Test.Store.Core.Security
+{
+    internal class AuthCacheServiceTest
+    {
+    }
+}
